7b4231b88a0d8f39cac52a63da50176e449ef396
Henry <khanhphamvk1996@gmail.com>
2023-04-19 20:08:19 +0700

    Init Project

---
+++ app/src/main/java/com/fossil/technical/gallery/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fossil.technical.gallery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
